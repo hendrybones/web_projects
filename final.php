46c69707c09b5c73599747f2502a2fbdd776bd49
mr bones <bones@localhost.localdomain>
2020-02-05 21:41:39 +0300

    my portfolio

---
+++ final.php
@@ -0,0 +1,319 @@
+<!Doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>best website</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="onepage.css" />
+    <script src="main.js"></script>
+
+     <!-- CSS -->
+     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.6/css/all.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="onepage.css">
+        <link rel="stylesheet" href="assets/css/media-queries.css">
+  <!-- Favicon and touch icons -->
+  <link rel="shortcut icon" href="assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+        <!-- end of the page's code -->
+        <!-- Javascript -->
+        <script src="assets/js/jquery-3.2.1.min.js"></script>
+        <script src="assets/js/jquery-migrate-3.0.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/retina-1.1.0.min.js"></script>
+        <script src="assets/js/waypoints.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+ <!-- Top menu -->
+ <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+      width: 100%;
+      height: 40%;
+  }
+  .h1{
+    text-align: center;
+    background: solid black;
+  }
+  .id-footer{
+    border: 5px solid red;
+  }
+  </style>
+</head>
+
+<body>
+<header>
+<nav class="navbar navbar-dark fixed-top navbar-expand-md navbar-no-bg">
+    <div class="container">
+        <a class="navbar-brand" href="onepage.php">Bootstrap 4 One Page Website Tutorial</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link scroll-link" href="#top-content">home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link" href="#skills">skills</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link" href="#about-us">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link" href="#portfolio">careers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link" href="#testimonials">experience</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link scroll-link" href="#blog">Blog</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</header>
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="2.jpeg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h2>WELECOME TO HENDRY MWAMBURI PORTFOLIO</h2>
+        <p>this is my personal website</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="4.jpeg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+      <h2>WELECOME TO HENDRY MWAMBURI PORTFOLIO</h2>
+        <p>this is my personal website</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="3.jpeg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+      <h2>WELECOME TO HENDRY MWAMBURI PORTFOLIO</h2>
+        <p>this is my personal website</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<!--- skills-->
+<div id="skills" class="container-fluid bg-warning" style="padding-top:70px;padding-bottom:70px">
+<div class="services-container section-container">
+    <div class="container">
+        <div class="row">
+            <div class="col services section-description wow fadeIn">
+            <h2><a href="#services">MY SKILLS</a></h2>
+                <div class="divider-1 wow fadeInUp"><span></span></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 services-box wow fadeInUp">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="services-box-icon">
+                            <i class="fas fa-magic"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Web Design</h3>
+                        <p></p>
+                    </div>
+                </div>Lorem ipsum dolor sit am
+            </div>
+            <div class="col-md-4 services-box wow fadeInDown">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="services-box-icon">
+                            <i class="fas fa-cog"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h3>UI / UX</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 services-box wow fadeInUp">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="services-box-icon">
+                            <i class="fab fa-google"></i>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <h3>Marketing</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- About us -->
+<div id="about-us" class="container-fluid bg-warning" style="padding-top:70px;padding-bottom:70px">
+<div class="about-us-container section-container section-container-gray-bg">
+    <div class="container">
+        <div class="row">
+        <div class="col-12 col-lg-5 about-us-box wow fadeInUp">
+                <div class="about-us-box-img">
+                    <!--<img src="10.jpeg" alt="about" data-at2x="assets/img/about/1.jpg">-->
+                    <img src="mwam.jpg" class="img-fluid rounded-circle" alt="v deseal" width="500" height="450">
+                </div>
+            </div>
+            <div class="col-12 col-lg-7 about-us-box wow fadeInLeft">
+                <div class="about-us-box-text">
+                    <h2><a href="#services">About hendry</a></h2>
+                    <p class="medium-paragraph">
+                        Student at Taita taveta University  studies bachelor of business information technology(BBIT)
+
+                    </p>
+                    <p>
+                        I am developer first started as web developer learning some basic in php to implement back end system of a website. Then moved to java script and its frame currently working on node js.
+
+                    </p>
+                    <p>
+                        I am working on an android project planning to perfect my skills on android development. i also have some basic skils on python rograming language using it also as back end in a website
+
+                    </p>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+</div>
+<div id="portfolio" class="container-fluid bg-warning" style="padding-top:70px;padding-bottom:70px">
+<div class="portfolio-container section-container">
+    <div class="container">
+    <div id="#services">
+        <div class="row">
+            <div class="col portfolio section-description wow fadeIn">
+                <h2><a href="#services">MY PORTFOLIO</a></h2>
+        
+                <div class="divider-1 wow fadeInUp"><span></span></div>
+                <p>The following are the projects which i am working on and some i have already complited</p>
+            </div>
+        </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 portfolio-box wow fadeInUp">
+                <div class="portfolio-box-image">
+                    <img src="13.jpeg" alt="" data-at2x="13.jpeg">
+                </div>
+                <h3><a href="#">ANDROID PROJECTS</a> <i class="fas fa-angle-right"></i></h3>
+                <div class="portfolio-box-date"><i class="far fa-calendar"></i> March 2019</div>
+                <p>these are some of the projects i have been working on for android development.</p>
+            </div>
+            <div class="col-md-4 portfolio-box wow fadeInDown">
+                <div class="portfolio-box-image">
+                    <img src="17.jpeg" alt="" data-at2x="17.jpeg">
+                </div>
+                <h3><a href="#">WEB design</a> <i class="fas fa-angle-right"></i></h3>
+                <div class="portfolio-box-date"><i class="far fa-calendar"></i> February 2018</div>
+                <p>here the website which am working on </p>
+            </div>
+            <div class="col-md-4 portfolio-box wow fadeInUp">
+                <div class="portfolio-box-image">
+                    <img src="8.jpeg" alt="" data-at2x="8.jpeg">
+                </div>
+                <h3><a href="#">thinking of cloud </a> <i class="fas fa-angle-right"></i></h3>
+                <div class="portfolio-box-date"><i class="far fa-calendar"></i> January 2020</div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col section-bottom-button wow fadeInUp">
+                <a class="btn btn-primary btn-link-3" href="#">Load more</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="#blog">
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+<h3>my blogs</h3>
+</div>
+</div>
+</div>
+</div>
+<!--- blogs--->
+<div class="container">
+
+<div class="row">
+<div class="col-md-6">
+<h3>blogs</h3>
+</div>
+<div class="col-md-6">
+<h3>blogs</h3>
+</div>
+</div>
+</div>
+</div>
+<!---footer--->
+<div class="container">
+<div id="footer">
+<div class="row">
+<div class="col-md-4">
+<!--social media-->
+<h3>social media</h3>
+</div>
+<div class="col-md-4">
+<h3>CONTACT US</h3>
+  <form action="/action_page.php">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+    </div>
+    <div class="form-group">
+      <label for="pwd">comments:</label>
+      <input type="textarea" class="form-control" id="pwd" textarea="Enter password" name="pswd">
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember"> Remember me
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+
+</div>
+</div>
+<div class="col-md-4">
+<h3>links</h3>
+</div>
+<p> COPYRIGHT @2020</P>
+</div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
